6f8f768f8b193d038f5ac0daaa2209ad8d28161f
ReaZzy <maxim.nebela@gmail.com>
2026-01-19 22:32:12 +0100

    fix: rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -5,17 +5,21 @@ import { ME } from "@/constants/me";
 
 export async function GET(context: APIContext) {
   const posts = getBlogPosts();
+  const site = context.site ?? new URL("https://nebela.dev");
 
   return rss({
     title: `${ME.name}'s blog`,
     description: `Articles and thoughts from ${ME.name}`,
-    site: context.site ?? "nebela.dev",
+    site: site,
+    xmlns: {
+      atom: "http://www.w3.org/2005/Atom",
+    },
     items: posts.map((post) => ({
       title: post.title,
       pubDate: post.date,
       description: post.description,
       link: `/blog/${post.slug}/`,
     })),
-    customData: `<language>en-us</language>`,
+    customData: `<language>en-us</language><atom:link href="${new URL("rss.xml", site)}" rel="self" type="application/rss+xml"/>`,
   });
 }
